a00dfecd80f6503e820f84e1ab7251db164a1bae
Michael <muniu.dev@gmail.com>
2023-10-03 09:19:24 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+//Sol 1
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-const num1 = 31;
-const num2 = 2;
-const multiply = num1*num2;
+//Sol 2
+const random = Math.floor(Math.random() * 10 + 1)
 
-const random = Math.floor(Math.random() * 10) + 1;
+//Sol 3
+let num3 = 14;
+let num4 = 5;
+const mod = num3 % num4;
 
-const num3 = 14;
-const num4 = 5;
-const mod = (num3 % num4);
-
-const max = Math.max(5,10,15,20);
+//Sol 4
+const max = Math.max(3,5,7,9,15,20,4);
